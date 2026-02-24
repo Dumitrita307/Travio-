385c06244cf3e0e6005164de99ffdc26ec87385d
unknown <123buruiana456@gmail.com>
2026-02-24 11:58:49 +0200

    Am adaugat sectiunea de contact si un raspuns la logare

--- script.js
+++ script.js
@@ -33,3 +33,44 @@ document.addEventListener("DOMContentLoaded", function() {
         .catch(error => console.error("Eroare la încărcarea JSON:", error));
 
 });
+
+const openFormBtn = document.getElementById("openFormBtn");
+const formContainer = document.getElementById("formContainer");
+const submitBtn = document.getElementById("submitBtn");
+
+openFormBtn.addEventListener("click", function() {
+    formContainer.classList.toggle("active");
+});
+const travelToast = document.getElementById("travelToast");
+const travelMessage = document.getElementById("travelMessage");
+const travelSubMessage = document.getElementById("travelSubMessage");
+
+submitBtn.addEventListener("click", function() {
+    const name = document.getElementById("nameInput").value.trim();
+    const email = document.getElementById("emailInput").value.trim();
+
+    if (name !== "" && email !== "") {
+
+        travelMessage.innerHTML = `${name}, te vom anunța când apar noi destinații de explorat! 🌍`;
+       
+
+        travelToast.classList.add("show");
+
+        // dispare automat după 4 secunde
+        setTimeout(() => {
+            travelToast.classList.remove("show");
+        }, 4000);
+
+        // reset
+        document.getElementById("nameInput").value = "";
+        document.getElementById("emailInput").value = "";
+        formContainer.classList.remove("active");
+
+    } else {
+        alert("Te rog completează toate câmpurile!");
+    }
+});
+
+closeMessage.addEventListener("click", function() {
+    successMessage.classList.remove("active");
+});
